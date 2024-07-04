ec661120539bd3954f7859285d334757b8cdf34f
Niranjan Sarpangala <34322933+nsarpangala@users.noreply.github.com>
2024-07-04 10:54:21 -0400

    update fiber creation - with crosslinkers
    update fiber creation - with crosslinkers

--- filament_network.py
+++ filament_network.py
@@ -2,6 +2,7 @@ import numpy as np
 from ball_spring_class import Ball, Spring
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
+import random
 
 class FilamentNetwork:
     def __init__(self, num_balls, area_size, max_connections=3):
@@ -120,3 +121,78 @@ class Fiber:
         ball1.applyForce(np.array([1e-3, 0.0]))  # Example: External force
         for spring in self.springs:
             spring.update()
+
+
+
+class FiberNetwork2:
+    def __init__(self, number_of_filaments, area_size, num_segments, segment_length, deviation=0.75):
+        self.balls = []
+        self.springs = []
+        self.fibers = []
+        self.crosslinks = []
+        self.area_size = area_size
+        self.number_of_filaments = number_of_filaments
+        self.num_segments = num_segments
+        self.segment_length = segment_length
+        self.deviation = deviation
+        self.color_map = get_cmap('viridis', number_of_filaments)
+    def create_filaments(self):
+        for i in range(self.number_of_filaments):
+            fib = Fiber(self.area_size, self.num_segments, self.segment_length, self.deviation)
+            self.fibers.append(fib)
+            self.balls.extend(fib.balls)
+            self.springs.extend(fib.springs)
+    def ceate_crosslinks_end_to_end(self):
+        "connect randomly"
+        for i in range(self.number_of_filaments):
+            for j in range(i+1, self.number_of_filaments):
+                # Connect the first ball of the i-th fiber with the first ball of the j-th fiber
+                crosslink = Spring(self.fibers[i].balls[-1], self.fibers[j].balls[0])
+                self.crosslinks.append(crosslink)
+                self.springs.append(crosslink)
+    
+    def ceate_crosslinks_random(self, max_crosslinks=10):
+        "connect randomly"
+        created_crosslinks = 0
+        while created_crosslinks < max_crosslinks:
+            # Randomly select two different fibers
+            i, j = random.sample(range(self.number_of_filaments), 2)
+
+            # Randomly select a ball from each fiber
+            ball_from_i = random.choice(self.fibers[i].balls)
+            ball_from_j = random.choice(self.fibers[j].balls)
+
+            # Create a crosslink if not already exists
+            if not any(cl.ball1 == ball_from_i and cl.ball2 == ball_from_j for cl in self.crosslinks):
+                crosslink = Spring(ball_from_i, ball_from_j)
+                self.crosslinks.append(crosslink)
+                self.springs.append(crosslink)
+                created_crosslinks += 1
+
+    def update(self):
+        ball1 = self.balls[0]
+        ball1.applyForce(np.array([1e-3, 0.0]))  # Example: External force
+        for spring in self.springs:
+            spring.update()
+
+    def display(self, ax):
+        ax.clear()
+        ax.set_xlim(0, self.area_size)
+        ax.set_ylim(0, self.area_size)
+
+        for idx, fiber in enumerate(self.fibers):
+            color = self.color_map(idx) 
+            # for segment in fiber.segments:
+            #     circle = plt.Circle(segment.position, segment.radius, color='r')
+            #     ax.add_artist(circle)
+            for filament in fiber.springs:
+                line = plt.Line2D((filament.ball1.position[0], filament.ball2.position[0]),
+                                (filament.ball1.position[1], filament.ball2.position[1]), lw=1, color=color)
+                ax.add_artist(line)
+        for crosslink in self.crosslinks:
+            line = plt.Line2D((crosslink.ball1.position[0], crosslink.ball2.position[0]),
+                            (crosslink.ball1.position[1], crosslink.ball2.position[1]), lw=1,ls='--', color='k')
+            ax.add_artist(line)
+        
+
+        
